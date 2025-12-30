e18eb1f036d708922b4221c342fe63fd7756c74b
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2025-12-30 18:14:55 +0100

    Fix syntax error in for.py and improve output formatting

--- for.py
+++ for.py
@@ -1,5 +1,6 @@
 
 # find all numbers > 10 and count the occurances
+print("-----")
 
 listOfNumbers = [1,3,5,6,10,234,44,55,66,77]
 limit = 10 # limit of the for
@@ -7,4 +8,22 @@ count = 0 # occurances
 sum = 0 # value of the occurances
 
 for n in listOfNumbers:
-    if  n > 10 
+    if  n > limit:
+        count += 1
+        sum += n
+        
+print("count = ", count)
+print("sum = ", sum)
+print("-----")
+
+
+# sum all the possibile combination
+
+prevValue = 0
+
+for n in listOfNumbers:
+    prevValue += n
+
+print(prevValue)
+
+
