15aac00c35667e57dc9ee6d0587c67a3086eea3a
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2025-12-30 18:05:06 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+numeri = [1,2,3,4,5]
+nomi = ["marco", "luca","giovanni"]
+print(nomi[1])
+nomi[1]= "modificato"
+print(nomi[1])
+nomi.append("annamaria")
+print(nomi)
+nomi.remove("annamaria")
+print(nomi)
